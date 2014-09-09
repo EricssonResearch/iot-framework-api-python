cd3b244b843d940d3204ee685cee0600c9565e60
Alberto Blazquez <hola@albertoblazquez.net>
2014-09-09 15:08:50 +0200

    Add basic functions for users and streams.
    Each 'resource' should rely on the auxiliary api.py module to make API calls.

---
+++ datapoints.py
@@ -0,0 +1,10 @@
+import api
+
+def get_datapoint(id):
+	pass
+
+def post_datapoint():
+	pass
+
+# def post_datapoint_async():
+# 	pass
---
+++ main.py
@@ -0,0 +1,12 @@
+#! /bin/python
+
+
+if __name__ == '__main__':
+	print "Welcome to the IoT Framework's Python API"
+
+	# Look up the access_token
+	# Else, look up the username / password
+	# Else
+	# start()
+
+
